89c30661c1ccead348fa8d345b75586de4d6e780
Kai Aetherweigh <136946134+LeonardBenjamin@users.noreply.github.com>
2024-08-29 19:20:00 +0700

    .

---
+++ Lesson 1/app.py
@@ -0,0 +1,21 @@
+# name = input("Enter your name: ")
+# score = int(input("Enter your score: "))
+
+# if score >= 90:
+#     print(name, "your grade is A")
+# elif score >= 80:
+#     print(name, "your grade is B")
+# elif score >= 70:
+#     print(name, "your grade is C")
+# elif score >= 60:
+#     print(name, "your grade is D")
+# else:
+#     print(name, "your grade is F")
+
+# for i in range(10):
+#     print(i)
+
+i = 0
+while i < 10:
+    print(i)
+    i += 1
---
+++ Lesson 2/app.py
@@ -0,0 +1,13 @@
+# subject = ["Maths", "English", "Science","Liturature","Physics"]
+# print(subject[1])
+# print(len(subject))
+# print(subject[-1])
+# subject.append([1,2,3])
+# print(subject)
+# for i
+
+def giaytrungtuyen(name,school):
+    print(f"Chúc mừng bạn {name} trúng tuyển {school}.")
+
+giaytrungtuyen("Huy","HUST")
+giaytrungtuyen("Minh", "DHQT")
